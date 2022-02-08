efb49d9f1ccfa502941d7a49267054afcdf3311b
SergiuPert <sergiupert@gmail.com>
2022-02-08 14:45:30 +0200

    patch 0.2

--- data_manager.py
+++ data_manager.py
@@ -10,11 +10,13 @@ def get_boards(cursor):
     cursor.execute(query)
     return cursor.fetchall()
 
+
 @database_connection.connection_handler
 def get_cards_by_board_id(cursor, board_id):
     query = """
         SELECT *
         FROM cards
-        WHERE cards.board_id == %(b_id)s
+        WHERE board_id = %(board_id)s
     ;"""
-    cursor.execute(query)
+    cursor.execute(query, {"board_id": board_id})
+    return cursor.fetchall()
--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,85 @@ function init() {
 }
 
 init();
+
+
+//
+// async function apiGet(url) {
+//     let response = await fetch(url);
+//     if (response.ok) {
+//         let data = await response.json();
+//         return data;
+//     }
+// }
+//
+// async function apiPost(url, payload) {
+//     let response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(payload),
+//     });
+//     if (response.ok) {
+//         let data = await response.json();
+//         // console.log(data)
+//         return data;
+//     }
+// }
+//
+// async function apiDelete(url, payload) {
+//     let response = await fetch(url, {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(payload),
+//     });
+//     if (response.ok) {
+//         let data = await response.json();
+//         return data;
+//     }
+// }
+//
+// async function apiPut(url, payload) {
+//     let response = await fetch(url, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(payload),
+//     });
+//     if (response.ok) {
+//         let data = await response.json();
+//         return data;
+//     }
+// }
+//
+// async function get_boards() {
+//     let boards = await apiGet("/api/boards");
+//     // console.log(boards);
+//     let boardsDiv = document.getElementById("boards_div");
+//     for(let index=0; index < boards.length; index++) {
+//         console.log(boards[index])
+//         boardsDiv.innerHTML += `
+//         <div class="board">
+//             <h1>${boards[index].title}</h1>
+//
+//         </div>
+//     `;
+//     }
+// }
+//
+//
+// async function get_cards() {
+//
+// }
+//
+//
+//
+// async function init_page() {
+//     await get_boards();
+// }
+//
+//
+// await init_page();
